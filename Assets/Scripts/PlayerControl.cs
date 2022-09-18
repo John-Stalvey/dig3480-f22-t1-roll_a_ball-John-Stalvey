83f38c7001bd93db2225f9bcd9c7830198748f44
John-Stalvey <112122528+John-Stalvey@users.noreply.github.com>
2022-09-18 19:30:17 +0000

    Sep 18, 2022, 3:30 PM

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -60,7 +60,7 @@ public class PlayerControl : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    
     {
         if(other.gameObject.CompareTag("PickUp Bad"))
         {
