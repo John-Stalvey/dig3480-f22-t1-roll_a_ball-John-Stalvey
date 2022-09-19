4f968377b863bc99cb1ee9ecf7680c1cd2a3b397
John-Stalvey <112122528+John-Stalvey@users.noreply.github.com>
2022-09-19 02:52:53 +0000

    Sep 18, 2022, 10:52 PM

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -45,7 +45,7 @@ public class PlayerControl : MonoBehaviour
         countText.text = "Count: " + count.ToString();
         if(count == 6) 
         {
-            transform.position = new Vector3()
+            transform.position = new Vector3(56, 0.05, 0.25);
             winTextObject.SetActive(true);
         }
         
